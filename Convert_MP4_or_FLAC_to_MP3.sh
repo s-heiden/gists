5288502a33690b6c230f2ae02bf79bb8ab7f16c7
Stefan Heiden <stefan.w.heiden@gmail.com>
2019-05-01 15:09:36 +0200

    remove duplication

--- Convert_MP4_or_FLAC_to_MP3.sh
+++ Convert_MP4_or_FLAC_to_MP3.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-for f in *.m4a; do ff=${f%.m4a}.mp3; ffmpeg -i "$f" -codec:a libmp3lame -q:a 0 -map_metadata 0 "$ff"; done
-for f in *.flac; do ff=${f%.flac}.mp3; ffmpeg -i "$f" -c:a libmp3lame -q:a 0 -map_metadata 0 "$ff"; done
+#
+# title:            Convert MP4 or FLAC to MP3
+# author:           s-heiden
+# date:             2019-05-01
+# requirements:     lame
+shopt -s extglob
+for filename in *(*.mp4|*.flac); do
+    new_filename=${filename%.*}.mp3
+    echo Converting $filename to $new_filename
+    ffmpeg -i "$filename" -codec:a libmp3lame -q:a 0 -map_metadata 0 "$new_filename"
+done
