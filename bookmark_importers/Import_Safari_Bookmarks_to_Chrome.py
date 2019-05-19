59743d2204231f7aa743157c8760fda7f7fa5657
Stefan Heiden <stefan.w.heiden@gmail.com>
2019-05-19 15:11:52 +0200

    add first version of working importer for Safari bookmarks

--- bookmark_importers/Import_Safari_Bookmarks_to_Chrome.py
+++ bookmark_importers/Import_Safari_Bookmarks_to_Chrome.py
@@ -1,153 +1,99 @@
 # !/usr/bin/env python
 #
-# Title:            Import_Chrome_Bookmarks_To_Safari.py
-# Author:           binss, s-heiden
+# Title:            Import_Safari_Bookmarks_To_Chrome.py
+# Author:           s-heiden
 # Setup:            make sure, terminal has full disk access: http://osxdaily.com/2018/10/09/fix-operation-not-permitted-terminal-error-macos/
 #
+import codecs
+import datetime
+import json
+import os
+import shutil
+import sys
 
 from biplist import *
-import json
-import datetime
-import os
-import sys
-import shutil
-
-
-class BookmarkManager:
-    def __init__(self):
-        self.bookmarks = []
-        self.number = 0
-
-    def load_from_safari(self, raw):
-        # TODO
-        print "Loading from Safari"
-
-    def save_to_chrome(self, raw):
-        print "Saving to Chrome"
-
-    def get_number(self):
-        return self.number
 
 
 def main():
     home_path = os.environ['HOME']
-
-    safari_path = home_path + "/Library/Safari/Bookmarks.plist"
-    chrome_path = home_path + "/Library/Application Support/Google/Chrome/Default/Bookmarks"
+    safari_path = home_path + '/Library/Safari/Bookmarks.plist'
+    chrome_path = home_path + '/Library/Application Support/Google/Chrome/Default/Bookmarks'
 
     if not os.path.exists(safari_path):
-        print "[Error] The bookmark files of safari do not exist! path: " + safari_path
+        print '[Error] The bookmark files of safari do not exist! path: ' + safari_path
         return
 
     if not os.path.exists(chrome_path):
-        print "[Error] The bookmark files of chrome do not exist! path: " + chrome_path
+        print '[Error] The bookmark files of chrome do not exist! path: ' + chrome_path
         return
 
-    bookmark_manager = BookmarkManager()
-
-    with open(safari_path, "r") as f:
+    with open(safari_path, 'r') as f:
         try:
             safari_bookmarks = readPlist(safari_path)
-            # print safari_bookmarks
-            chrome_bookmarks = parse(safari_bookmarks)
-            print chrome_bookmarks
+            chrome_bookmarks = convert(safari_bookmarks)
+            create_backup(chrome_path)
+            with codecs.open(chrome_path, 'w+', encoding="utf-8") as fp:
+                json.dump(chrome_bookmarks, fp, ensure_ascii=False)
+
         except (InvalidPlistException, NotBinaryPlistException), e:
-            print "Not a plist:", e
+            print 'Not a plist:', e
 
 
-def uri_dictionary_to_bookmark_title(object):
-    title_key = "title"
-    if title_key in object:
-        return object[title_key]
-    else:
-        print 'Warning: No title element in URIDictionary.'
+def create_backup(chrome_path):
+    backup_path = chrome_path + '.' + datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S') + '.bak'
+    print 'Creating backup of Chrome bookmarks at ' + backup_path + '...'
+    shutil.copyfile(chrome_path, backup_path)
 
 
-#
-#
-#
-def parse(safari_bookmarks):
+def convert(safari_bookmarks):
     children_node = safari_bookmarks['Children']
-    print children_node
-
-    chrome_bookmarks = {
+    return {
         'version': 1,
         'roots': {
             'synced': {
-                'type': 'Folder',
-                'children': []
+                'children': [],
+                'type': 'folder'
             },
-            'other': get_other(children_node),
-            'bookmark_bar': get_bookmark_bar(children_node)
+            'other': {
+                'type': 'folder',
+                'children': map(to_chrome_node, elems_other_than(lst=children_node, key='Title', value='BookmarksBar'))
+            },
+            'bookmark_bar': to_chrome_node(first_child_having(parent=children_node, key='Title', value='BookmarksBar'))
         }
     }
-    return chrome_bookmarks
 
 
-#
-#
-#
-def get_other(children_node):
-    # TODO: Implement!
+def to_chrome_node(safari_node):
+    # handle ignored safari nodes
+    if 'WebBookmarkIdentifier' in safari_node and safari_node['WebBookmarkIdentifier'] == 'History':
+        print 'Ignoring Safari history...'
+    elif 'Title' in safari_node and safari_node['Title'] == 'com.apple.ReadingList':
+        print 'Ignoring Safari reading list...'
+    # handle leaves
+    elif 'WebBookmarkType' in safari_node and safari_node['WebBookmarkType'] == 'WebBookmarkTypeLeaf':
+        return {
+            'type': 'url',
+            'url': safari_node['URLString'],
+            'name': safari_node['URIDictionary']['title']
+        }
+    # handle lists
+    elif safari_node['WebBookmarkType'] == 'WebBookmarkTypeList':
+        return {
+            'type': 'folder',
+            'name': safari_node['Title'],
+            'children': map(to_chrome_node, safari_node['Children']) if 'Children' in safari_node else {}
+        }
     return {}
 
 
-#
-#
-#
-def get_bookmark_bar(children_node):
-    # TODO: Complete!
-    title_key = 'title'
-    bookmarks_bar_title = 'BookmarksBar'
-
-    bookmarks_bar_node = filter(
-        lambda child_node: title_key in child_node and child_node[title_key] == bookmarks_bar_title,
-        children_node
-    )
-
-    bookmark_bar = {
-        'children': [],
-        'type': 'folder'
-    }
-
-    return bookmark_bar
+def first_child_having(parent, key, value):
+    return filter(lambda child_node: (key in child_node and child_node[key] == value), parent)[0]
 
 
-# TODO: delete after completion
-def iterate_children(object):
-    #
-    # define constants
-    #
-    children = "Children"
-    title = "Title"
-    should_omit_from_ui = "ShouldOmitFromUI"
-    url_string = "URLString"
-    web_bookmark_identifier = "WebBookmarkIdentifier"
-    web_bookmark_type = "WebBookmarkType"
-    uri_dictionary = "URIDictionary"
-
-    if should_omit_from_ui in object and object[should_omit_from_ui] == True:
-        return
-    if web_bookmark_type in object and object[web_bookmark_type] == "WebBookmarkTypeProxy":
-        return
-    if title in object:
-        print "\nTitle = " + object[title]
-        # merke dir den title
-    if web_bookmark_type in object and object[web_bookmark_type] != "WebBookmarkTypeLeaf":
-        print "  WebBookmarkType = " + object[web_bookmark_type]
-    if url_string in object:
-        print "  " + object[url_string]
-    if web_bookmark_identifier in object:
-        print "  WebBookmarkIdentifier = " + object[web_bookmark_identifier]
-
-    #
-    if uri_dictionary in object:
-        uri_dictionary_to_bookmark_title(object[uri_dictionary])
-    if children in object:
-        for obj in object[children]:
-            iterate_children(obj)
+def elems_other_than(lst, key, value):
+    return filter(lambda child_node: (key not in child_node or child_node[key] != value), lst)
 
 
 if __name__ == '__main__':
-    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "site-packages"))
+    sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'site-packages'))
     main()
