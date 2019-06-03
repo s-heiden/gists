31e37d288ca52b596d8b56acc99bd22705dd4982
Stefan Heiden <stefan.heiden@iteratec.com>
2019-06-03 13:29:47 +0200

    merge bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,3 +1,4 @@
 alias etron="cd /Volumes/\[C\]\ Windows\ 10\ Education\ 64-bit\ English/Users/stefan/etron-reporting/etron-reporting"
 alias gists="cd ~/Repositories/gists"
 alias matlab='/Applications/MATLAB_R2018b.app/bin/matlab -nodesktop -nosplash $*'
+alias sourcetree="/Applications/Sourcetree.app/Contents/MacOS/Sourcetree . &"
