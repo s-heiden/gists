d52437b95ded1bffdcab7b4673edef2a6dd3734d
Stefan Heiden <stefan.w.heiden@gmail.com>
2019-06-06 11:57:45 +0200

    rename shell scripts according to naming convention and move executable shell scripts to "bin" folder

--- Replace_Space_Character_in_Files_of_Directory_By_Underscore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# author:           s-heiden
-# name:             Replace_Space_Character_in_Files_of_Directory_By_Underscore.sh
-#
-for file in * ; do
-    mv "$file" "${file// /_}"
-done
